47703fc4266cabf9c8786fd167befa7d34a5a89f
ANILCAN <ANILCAN@DESKTOP-AO2J3TE>
2021-10-06 04:14:26 +0300

    Add project files.

---
+++ SecuringWebApiUsingJwtAuthentication/Entities/CustomersDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SecuringWebApiUsingJwtAuthentication.Entities
+{
+    public class CustomersDbContext : DbContext
+    {
+        public CustomersDbContext(DbContextOptions<CustomersDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Order>().ToTable("Order");
+        }
+    }
+}
---
+++ SecuringWebApiUsingJwtAuthentication/Interfaces/ICustomerService.cs
@@ -0,0 +1,10 @@
+﻿using SecuringWebApiUsingJwtAuthentication.Requests;
+using System.Threading.Tasks;
+
+namespace SecuringWebApiUsingJwtAuthentication.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<LoginResponse> Login(LoginRequest loginRequest);
+    }
+}
